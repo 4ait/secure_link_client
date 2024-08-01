5f5ea076e32d15f3d089d75e4929d6bfde1cdb5f
Ridtt <ridtt.tz@gmail.com>
2024-08-01 21:37:27 +1000

    check ver

--- src/main.rs
+++ src/main.rs
@@ -9,26 +9,27 @@ use tokio_rustls::rustls::{ClientConfig, RootCertStore};
 use tokio_rustls::rustls::pki_types::{CertificateDer, ServerName};
 use tokio_rustls::{TlsConnector, TlsStream};
 use x509_parser::pem::{Pem};
+use x509_parser::x509::X509Version;
 
 async fn load_dev_cert(cert_path: &str) -> Result<Vec<CertificateDer>, Box<dyn std::error::Error>> {
-   
+
     let mut cert_file = File::open(cert_path).await?;
     let mut data = Vec::new();
     cert_file.read_to_end(&mut data).await?;
-    
-    let der_certs: Vec<CertificateDer> = 
+
+    let der_certs: Vec<CertificateDer> =
         Pem::iter_from_buffer(&data).map(|pem| {
-        
+
             let pem = pem.expect("Reading next PEM block failed");
             let x509 = pem.parse_x509().expect("X.509: decoding DER failed");
-            
-            println!("{}", x509.subject);
-    
+
+            assert_eq!(x509.tbs_certificate.version, X509Version::V3);
+
             CertificateDer::from(pem.contents)
-        
+
         }).collect();
-    
-    
+
+
     Ok(der_certs)
 }
 
@@ -95,10 +96,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let config = Arc::new(config);
 
     let mut connection = connect_to_domain(config, socket_addr, "127.0.0.1".into()).await.unwrap();
-    
+
     let mut buff = [0; 1];
-    
+
     connection.read_exact(&mut buff).await.unwrap();
-    
+
     Ok(())
 }
