13d2bbcae41c82058e2e1ddf57e4a83fccaf2712
Ridtt <ridtt.tz@gmail.com>
2024-08-12 14:35:01 +1000

    folders

--- cross_compile_release.sh
+++ cross_compile_release.sh
@@ -1,8 +1,9 @@
 rustup update && \
 cargo install cross --git https://github.com/cross-rs/cross && \
-cross build --target aarch64-unknown-linux-gnu --release && \
-cross build --target x86_64-pc-windows-gnu --release --features aws-lc-sys && \
-mkdir -p target/secure_link_releases/linux64 && \
 mkdir -p target/secure_link_releases/win64 && \
-cp target/aarch64-unknown-linux-gnu/release/secure_link target/secure_link_releases/linux64/secure_link && \
-cp target/x86_64-pc-windows-gnu/release/secure_link.exe target/secure_link_releases/win64/secure_link.exe
+mkdir -p target/secure_link_releases/linux64 && \
+cross build --target x86_64-pc-windows-gnu --release --features aws-lc-sys && \
+cp target/x86_64-pc-windows-gnu/release/secure_link.exe target/secure_link_releases/win64/secure_link.exe &&\
+cross build --target aarch64-unknown-linux-gnu --release && \
+cp target/aarch64-unknown-linux-gnu/release/secure_link target/secure_link_releases/linux64/secure_link
+
